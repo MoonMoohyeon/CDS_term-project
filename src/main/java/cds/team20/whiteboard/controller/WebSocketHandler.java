304393569e89d8d17beeca68926244fca54a1fb2
Kyprosa <propolis200@gmail.com>
2024-05-01 01:52:56 +0900

    중간발표

--- src/main/java/cds/team20/whiteboard/controller/WebSocketHandler.java
+++ src/main/java/cds/team20/whiteboard/controller/WebSocketHandler.java
@@ -3,6 +3,8 @@ package cds.team20.whiteboard.controller;
 import cds.team20.whiteboard.entity.Figure;
 import cds.team20.whiteboard.service.FigureService;
 import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
@@ -15,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
+    private static final Logger log = LoggerFactory.getLogger(WebSocketHandler.class);
     private List<WebSocketSession> sessionList = new ArrayList<>();
     private final FigureService figureService;
     @Autowired
@@ -24,28 +27,35 @@ public class WebSocketHandler extends TextWebSocketHandler {
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        super.afterConnectionEstablished(session);
-        String name = session.getHandshakeHeaders().get("name").get(0);
+        log.info("세션 [ {} ]  연결됨", session.getId());
+        /*세션 저장*/
         sessionList.add(session);
-        sessionList.forEach(s-> {
-            try {
-                s.sendMessage(new TextMessage(name+"님께서 입장하셨습니다."));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
+//        super.afterConnectionEstablished(session);
+//        String name = session.getHandshakeHeaders().get("name").get(0);
+//        sessionList.add(session);
+        session.sendMessage(new TextMessage("Hello World"));
+//        sessionList.forEach(s-> {
+//            try {
+//                s.sendMessage(new TextMessage(name+"님께서 입장하셨습니다."));
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        });
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        super.handleTextMessage(session, message);
-        Gson gson = new Gson();
-        Figure figure = gson.fromJson(message.getPayload(), Figure.class);
-        figureService.createFigure(figure);
-        session.sendMessage(new TextMessage("Hello World"));
+        String payload = message.getPayload();
+        log.info("handleTextMessage is called...");
+        log.info("payload {}", payload);
+//        super.handleTextMessage(session, message);
+//        Gson gson = new Gson();
+//        Figure figure = gson.fromJson(message.getPayload(), Figure.class);
+//        figureService.createFigure(figure);
+//        session.sendMessage(new TextMessage("Hello World"));
         sessionList.forEach(s-> {
             try {
-                s.sendMessage(new TextMessage(gson.toJson(figure)));
+                s.sendMessage(new TextMessage(payload));
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -54,15 +64,17 @@ public class WebSocketHandler extends TextWebSocketHandler {
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        super.afterConnectionClosed(session, status);
+        log.info("{} 연결 끊김", session.getId());
         sessionList.remove(session);
-        String name = session.getHandshakeHeaders().get("name").get(0);
-        sessionList.forEach(s-> {
-            try {
-                s.sendMessage(new TextMessage(name+"님께서 퇴장하셨습니다."));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
+//        super.afterConnectionClosed(session, status);
+//        sessionList.remove(session);
+//        String name = session.getHandshakeHeaders().get("name").get(0);
+//        sessionList.forEach(s-> {
+//            try {
+//                s.sendMessage(new TextMessage(name+"님께서 퇴장하셨습니다."));
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        });
     }
 }
